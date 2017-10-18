270e99dba62c3117eda2ee753ddff505fe5582c2
Muhammad Ridho <31976490+elridho@users.noreply.github.com>
2017-10-18 08:41:45 +0700

    Add files via upload

---
+++ tugas1-04217043.py
@@ -0,0 +1,11 @@
+nama ="Muhammad Ridho"
+nim="04217043"
+usia=19
+print("Nama : "+nama)
+print("NIM : "+nim)
+print("Umur : ")
+print(usia)
+print("Umurku 5 tahun lagi")
+print(usia + 5)  
+
+           
